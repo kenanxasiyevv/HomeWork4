d0046b4c07f2a8044546506919421b9dc7e9c66d
kenanxasiyevv <kananakh@code.edu.az>
2022-12-28 21:49:55 +0400

    Add project files.

---
+++ HomeWoek4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HomeWoek4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil et: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int r1, r2;
+            int tmp;
+            bool isDifferenceDigit = true;
+            while (number != 0)
+            {
+                r1 = number % 10;
+                number /= 10;
+                tmp = number;
+
+                while (tmp != 0)
+                {
+                    r2 = tmp % 10;
+                    tmp /= 10;
+
+                    if (r1 == r2)
+                    {
+                        Console.WriteLine("muxtelif reqemli deyil");
+                        isDifferenceDigit = false;
+                        break;
+                    }
+
+                }
+                if (!isDifferenceDigit)
+                    break;
+            }
+            if (isDifferenceDigit)
+                Console.WriteLine("Muxtelif Reqemlidir");
+        }
+    }
+}
